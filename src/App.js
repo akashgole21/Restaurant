8ad98f3d1d35dd2086aa2b52cc3ae70d4c646632
Akash Gole <97158557+akashgole21@users.noreply.github.com>
2024-07-11 13:50:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/" element={<RestaurantList />} />
+          <Route path="/" exact element={<RestaurantList />} />
           <Route path="/create" element={<RestaurantCreate />} />
           <Route path="/detail" element={<RestaurantDetail />} />
           <Route path="/search" element={<RestaurantSearch />} />
