ffd379064f4fc3ffc1845807d29fba82f2426bf6
Akash Gole <97158557+akashgole21@users.noreply.github.com>
2024-07-11 21:09:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import RestaurantUpdate from "./components/RestaurantUpdate";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/restaurant">
+      <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           <Route exact path="/restaurant" element={<RestaurantList />} />
